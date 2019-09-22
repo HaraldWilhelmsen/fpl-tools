36ab9f11f5a5caf4aec553e99fde3ba0f9c6d03a
HaraldWilhelmsen <wil.harald@gmail.com>
2019-09-22 19:44:45 +0200

    add function to get info from PFL

--- src/data-fetch/fetch_data.py
+++ src/data-fetch/fetch_data.py
@@ -1,3 +1,22 @@
 import numpy as np
+import pandas as pd
+import json
+import requests
+from pandas.io.json import json_normalize
+
+def get_json():
+    r = requests.get('https://fantasy.premierleague.com/api/bootstrap-static/')
+    jsonResponse = r.json()
+    with open('fpl.json', 'w') as outfile:
+        #pass
+        json.dump(jsonResponse, outfile)
 
 
+def read_premierleague_data():
+    with open('fpl.json') as json_data:
+        d = json.load(json_data)
+    print(list(d.keys()))
+
+
+get_json()
+read_premierleague_data()
