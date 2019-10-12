fcfe8ca748877065fc54f0351d5f927a41a405a9
olemabo <olemartinbo@gmail.com>
2019-10-12 17:12:50 +0200

    update in plot of fixtures

--- src/fixture-planner/difficult.py
+++ src/fixture-planner/difficult.py
@@ -1,18 +1,15 @@
 import numpy as np
 import pandas as pd
 
-data  = pd.read_csv("difficulty.csv")
-
 def createDataFrame(txt):
     data = pd.read_csv(txt)
     for i in range(1,data.shape[1]):
         for j in range(data.shape[0]):
             num = str(i)
             data[num][j] = data[num][j].split(" ")
-
     return pd.DataFrame(data = data)
 	
-df = createDataFrame("difficulty.csv")
+df = createDataFrame("fixture_data/difficulty.csv")
 
 # one functon where all teams are evaluted and one where you specify
 # which teams to check
@@ -42,43 +39,21 @@ def best_games_future(df, GW_start, GW_end, best_teams):
         # maybe create a dataframe with score team and fixtures
         all_teams[teams] = team
     # sort the list according to the score
-    all_teams = insertionsort(all_teams)
+    all_teams = sort_teams_based_on_fixtures(all_teams)
     print("\n Best %i teams with fixtures from GW %i to %i" %(best_teams, GW_start, GW_end))
     for i in range(best_teams):
         print(all_teams[i])
     
-def insertionsort(A):
+def sort_teams_based_on_fixtures(A):
     for i in range(A.size - 1):
-        key = A[i + 1][0]
-        key2 = A[i + 1]
-        while i >= 0 and A[i][0] > key:
+        fixture_score = A[i + 1][0]
+        team_object = A[i + 1]
+        while i >= 0 and A[i][0] > fixture_score:
             A[i + 1] = A[i]
             i = i - 1
-        A[i + 1] = key2
+        A[i + 1] = team_object
     return A
 
-best_games_future(df, 1, 3, 3)
-
-#print("calc score: ", fixtureScoreOneTeam(df, 1, 1, 3))
-#fixtureScore(df, 1, 3)
-#print(fixtureScoreOneTeam(df, 0, 1, 3))
+#best_games_future(df, 1, 3, 3)
 
 
-
-
-"""
-print(data.shape, data['Team']=='ARS')
-print(data_pd)
-number_of_teams = data_pd.index[2] + 1
-print("number of teams: ", number_of_teams)
-
-print(data_pd['1'], data_pd['1'][0][4], data_pd.index)
-print(data_pd['Team'][:], data['Team'][:])
-# print by index smart to creat arrays
-print(np.array(data_pd[data_pd.index==0]))
-print(np.array(data_pd[data_pd['Team'] == 'ARS']))
-print(data_pd[data_pd['Team'] == 'ARS'])
-print(data_pd.loc[data_pd['Team'] == 'ARS'])
-print("\n", np.array(data_pd.loc[[1,2]])[1,1:])
-print("\n", np.array(np.array(data_pd.loc[[1,2]])[1,1:]).T.shape)
-"""
